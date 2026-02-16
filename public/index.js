71cbddcaf2435209eb597909472caf9989e91cc5
Abhishek09prajapati <abhishek091119@gmail.com>
2026-02-16 11:06:57 +0530

    dd

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // Static login folder
 app.use(express.static(path.join(__dirname)));
